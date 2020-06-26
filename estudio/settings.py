b2e0db693662adc6caa0ab1564d7e92dd51847d3
pabloSalva <pablo.salvador90@gmail.com>
2020-06-26 15:13:16 -0300

    arreglo archivo requirements

--- estudio/settings.py
+++ estudio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'naj*v056#c*&40tz7arjjkt!imwgoqd$u_lpl4h57uoz%v6yin'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
